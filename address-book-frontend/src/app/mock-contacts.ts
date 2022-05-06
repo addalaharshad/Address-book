cc1c0883c1aa3016f5638fbb4ee5f9851f0b4f05
addala harshad <64641316+addalaharshad@users.noreply.github.com>
2022-05-06 13:59:14 +0530

    Add files via upload

---
+++ address-book-frontend/src/app/mock-contacts.ts
@@ -0,0 +1,12 @@
+import { Contact } from './contacts';
+import { ChangeDetectorRef } from '@angular/core';
+
+export let CONTACTS: Contact[] = [
+  { id: 1,
+    name: 'Addala Harshad',
+    email : 'harshad.a@keka.com',
+    mobile: '+91 8179482316',
+    landline: '123456',
+    website: 'http://www.keka.com',
+    address: 'Madhapur, Hyderabad 500033' }
+];
